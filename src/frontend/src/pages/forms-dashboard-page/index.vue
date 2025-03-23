6f1cd5e0b6036475980a4381f1f80aea7bcfcc61
hscokies <hscokies@outlook.com>
2025-03-23 23:39:13 +0500

    feature/submissions-page: Add postfix for forms dashboard

---
+++ src/frontend/src/pages/forms-dashboard-page/index.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {RouterLink} from 'vue-router'
+import {ROUTES} from "@/app/router/index.js";
+import {Link, BarChart} from '@/shared/ui/icons'
+import { Table } from '@/shared/ui'
+import MockData from './data.json'
+
+const headers = [
+  {
+    key: 'id',
+    label: 'ID',
+  },
+  {
+    key: 'title',
+    label: 'Title',
+  },
+  {
+    key: 'subtitle',
+    label: 'Subtitle',
+  }
+]
+</script>
+
+<template>
+  <Table :columns="headers" :rows="MockData" has-actions>
+    <template #data="{ row }">
+      <RouterLink :to="`${ROUTES.Form}/${row.id}`" title="Get link" target="_blank">
+        <Link />
+      </RouterLink>
+      <RouterLink :to="`${ROUTES.Submissions}`" title="Get submissions">
+        <BarChart />
+      </RouterLink>
+    </template>
+  </Table>
+</template>
+
+<style scoped>
+</style>
