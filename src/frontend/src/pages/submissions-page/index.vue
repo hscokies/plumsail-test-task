7e118052f89d9b0ae72c63f12334a9b430b69654
hscokies <hscokies@outlook.com>
2025-03-23 23:39:26 +0500

    feature/submissions-page: Add submissions page

---
+++ src/frontend/src/pages/submissions-page/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import MockData from './data.json'
+import {RouterLink} from "vue-router";
+import {Button, BUTTON_SIZE, BUTTON_VARIANTS, Table} from '@/shared/ui'
+import {ROUTES} from "@/app/router/index.js";
+
+</script>
+
+<template>
+  <div class="submissions-page_root">
+    <div class="submissions-page_menu">
+      <Button :variant="BUTTON_VARIANTS.Secondary" :size="BUTTON_SIZE.Small" @click="$router.back()">
+        Back
+      </Button>
+    </div>
+    <Table
+      :columns="MockData.columns"
+      :rows="MockData.data"
+    />
+  </div>
+</template>
+
+<style scoped>
+.submissions-page_root{
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 8px;
+}
+</style>
